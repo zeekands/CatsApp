9f0b32751558a29c4399f0453905b84f079421b7
zeekands <aziz18ti@mahasiswa.pcr.ac.id>
2021-06-07 21:54:34 +0700

    remove unused files

--- core/src/main/java/com/zeekands/core/utils/AppExecutor.kt
+++ core/src/main/java/com/zeekands/core/utils/AppExecutor.kt
@@ -7,9 +7,7 @@ import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
 class AppExecutors @VisibleForTesting constructor(
-    private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
+    private val diskIO: Executor
 ) {
 
     companion object {
@@ -17,9 +15,7 @@ class AppExecutors @VisibleForTesting constructor(
     }
 
     constructor() : this(
-        Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
-        MainThreadExecutor()
+        Executors.newSingleThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
